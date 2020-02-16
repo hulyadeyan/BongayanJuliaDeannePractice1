60aa02c2035968765f5965b2065de0b400d68803
hulyadeyan <60645878+hulyadeyan@users.noreply.github.com>
2020-02-16 11:17:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function validate() {
+
+    var INPUTevents = document.getElementById("type").value;
+    var INPUTtitle = document.getElementById("title").value;
+    var INPUTdates = document.getElementById("dateFrom").value;
+    var INPUTdates2 = document.getElementById("dateTo").value;
+    var INPUTpost = document.getElementById("postDate").value;
+}
+    /*if(INPUTevents == ""  && INPUTtitle == "" && INPUTdates == "" 
+            && INPUTdates2 == "" && INPUTpost == ""){
+        alert("Please complete the form.");
+    }
+    else{
+        alert("You've successfully completed the form!");
+        }
+*/
